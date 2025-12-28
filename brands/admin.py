7ada1e34f01b734a0b7c45ec4ed5f386d6e2614e
Kaue Sobreira <sobreirak29@gmail.com>
2025-12-28 17:42:01 -0300

    feat: Register Brand model in admin with custom display and filter options

--- brands/admin.py
+++ brands/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Brand
 
-# Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description',)
+    list_filter = ('name',)
+
+admin.site.register(Brand, BrandAdmin)
