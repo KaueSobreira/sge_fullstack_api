655aa860b895670762f0ca29faa2dca79cc50674
Kaue <sobreirak29@gmail.com>
2026-01-20 18:18:20 -0300

    feat: Register signal to update product quantity when an outflow is created

---
+++ outflows/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Outflows
+from products.models import Product
+from django.core.exceptions import ValidationError
+
+
+@receiver(post_save, sender=Outflows)
+def update_product_quantity(sender, instance, created, **kwargs):
+    if created:
+        if instance.quantity > 0:
+            product = instance.product
+            product.quantity -= instance.quantity
+            product.save()
+        else:
+            raise ValidationError('A quantidade deve ser maior que 0')
