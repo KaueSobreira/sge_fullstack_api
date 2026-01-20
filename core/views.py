71d30a0f9ca355344cc7540876203c7219b68158
Kaue <sobreirak29@gmail.com>
2026-01-20 19:13:40 -0300

    feat: Add sales metrics calculation and integrate into home view and template

--- core/views.py
+++ core/views.py
@@ -6,8 +6,11 @@ def home(request):
 
     product_metrics = metrics.get_product_metrics()
 
+    sales_metrics = metrics.get_sales_metrics
+
     context = {
-        'product_metrics': product_metrics
+        'product_metrics': product_metrics,
+        'sales_metrics': sales_metrics
     }
 
     return render(request, 'home.html', context)
