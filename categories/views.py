2a31490bb3e430b491f49c6f9be5c9775d2e9166
Kaue <sobreirak29@gmail.com>
2026-01-22 18:02:32 -0300

    fix: Update permission strings for category views to follow the new naming convention

--- categories/views.py
+++ categories/views.py
@@ -10,7 +10,7 @@ class CategoryListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     template_name = 'category_list.html'
     context_object_name = 'categories'
     paginate_by = 10
-    permission_required = 'categories_view_category'
+    permission_required = 'categories.view_category'
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -27,22 +27,22 @@ class CategoryCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView
     template_name = 'category_create.html'
     form_class = forms.CategoryForm
     success_url = reverse_lazy('category_list')
-    permission_required = 'categories_add_category'
+    permission_required = 'categories.add_category'
 
 class CategoryDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = models.Category
     template_name = 'category_detail.html'
-    permission_required = 'categories_view_category'
+    permission_required = 'categories.view_category'
 
 class CategoryUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = models.Category
     template_name = 'category_update.html'
     form_class = forms.CategoryForm
     success_url = reverse_lazy('category_list')
-    permission_required = 'categories_change_category'
+    permission_required = 'categories.change_category'
 
 class CategoryDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = models.Category
     template_name = 'category_delete.html'
     success_url = reverse_lazy('category_list')
-    permission_required = 'categories_delete_category'
+    permission_required = 'categories.delete_category'
